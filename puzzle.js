a0a52cd49db750b0df6198af348e4711215bb3e2
Bogdan Jakovljević <b.jakovljevic21@gmail.com>
2023-01-24 23:13:14 +0100

    Adds Timer

--- puzzle.js
+++ puzzle.js
@@ -1,76 +1,114 @@
-var redovi = 3;
-var kolone = 3;
+const rowsNumber = 3
+const columnsNumber = 3
+const SECOND = 1000
 
-var currTile;
-var otherTile;
+let currTile
+let otherTile
 
-var broj_poteza = 0;
-window.onload = function(){
+let movesCounter = 0
+let timeRemaining = 10
 
+const initTable = () => {
+  for (let r = 0; r < rowsNumber; r++) {
+    for (let k = 0; k < columnsNumber; k++) {
+        let tile = document.createElement("img")
+        tile.src="./materijal/empty.jpg"
 
-    //inicijalizacija 3x3 table
-    for(let r = 0;r<redovi;r++){
-        for(let k=0;k<kolone;k++){
-            let tile = document.createElement("img");
-            tile.src="./materijal/prazno.jpg"
+        tile.addEventListener("dragstart", dragStart)
+        tile.addEventListener("dragover", dragOver)
+        tile.addEventListener("dragenter", dragEnter)
+        tile.addEventListener("dragleave", dragLeave)
+        tile.addEventListener("drop", dragDrop)
+        tile.addEventListener("dragend", dragEnd)
 
-            tile.addEventListener("dragstart",dragStart);
-            tile.addEventListener("dragover",dragOver);
-            tile.addEventListener("dragenter",dragEnter);
-            tile.addEventListener("dragleave",dragLeave);
-            tile.addEventListener("drop",dragDrop);
-            tile.addEventListener("dragend",dragEnd);
-            document.getElementById("delovi").append(tile);
-            document.getElementById("tabla").append(tile);
-        }
+        document.getElementById("pieces").append(tile)
+        document.getElementById("table").append(tile)
     }
+  }
+}
 
+const initPieces = () => {
+    const randomizedOrderOfPieces = getRandomizedOrderOfPieces()
+    for (let i = 0; i<randomizedOrderOfPieces.length ;i++) {
+        let tile = document.createElement("img")
+        tile.src= "./materijal/" + randomizedOrderOfPieces[i] + ".jpg"
 
-
-
-    //delovi
-    let delovi = []
-    for (let i=0;i<redovi*kolone;i++){
-        delovi.push(i.toString());
-    }
-
-    delovi.reverse();
-    for(let i=0;i<delovi.length;i++){
-        let j=Math.floor(Math.random() * delovi.length);
-
-        //zamena
-        let tmp=delovi[i];
-        delovi[i]=delovi[j];
-        delovi[j]=tmp;
-    }
-
-    for (let i=0;i<delovi.length;i++){
-        let tile=document.createElement("img");
-        tile.src="./materijal/" + delovi[i] + ".jpg"
-
-        //Drag funkcija
-        tile.addEventListener("dragstart",dragStart);
-        tile.addEventListener("dragover",dragOver);
-        tile.addEventListener("dragenter",dragEnter);
-        tile.addEventListener("dragleave",dragLeave);
-        tile.addEventListener("drop",dragDrop);
-        tile.addEventListener("dragend",dragEnd);
-        document.getElementById("delovi").append(tile);
+        tile.addEventListener("dragstart", dragStart)
+        tile.addEventListener("dragover", dragOver)
+        tile.addEventListener("dragenter", dragEnter)
+        tile.addEventListener("dragleave", dragLeave)
+        tile.addEventListener("drop", dragDrop)
+        tile.addEventListener("dragend", dragEnd)
+        document.getElementById("pieces").append(tile)
     }
 }
 
+const addListenerOnStartGameButton = () => {
+    const startGameButton = document.getElementById("start-game-button")
+    startGameButton.addEventListener("click", startGame)
+}
+
+const disableStartGameButton = () => {
+    const startGameButton = document.getElementById("start-game-button")
+    startGameButton.disabled = true
+}
+
+const getRandomizedOrderOfPieces = () => {
+  let randomizedOrderOfPieces = []
+  for (let i = 0; i < rowsNumber * columnsNumber; i++) {
+      randomizedOrderOfPieces.push(i.toString())
+  }
+
+  randomizedOrderOfPieces.reverse()
+  for(let i = 0; i < randomizedOrderOfPieces.length; i++){
+      let j = Math.floor(Math.random() * randomizedOrderOfPieces.length)
+
+      let tmp=randomizedOrderOfPieces[i]
+      randomizedOrderOfPieces[i]=randomizedOrderOfPieces[j]
+      randomizedOrderOfPieces[j]=tmp
+  }
+
+  return randomizedOrderOfPieces
+}
+
+const showConfirmDialog = () => {
+    if (confirm("No time remaining. Try again?")) {
+        location.reload()
+    }
+}
+
+const initTimer = () => {
+    setInterval(() => {
+        timeRemaining -= 1
+        document.getElementById("time-remaining").innerText = Math.max(0, timeRemaining)
+        if (!timeRemaining) {
+            showConfirmDialog()
+        }
+    }, SECOND)
+}
+
+const startGame = () => {
+    disableStartGameButton()
+    initPieces()
+    initTimer()
+}
+
+window.onload = () => {
+    initTable()
+    addListenerOnStartGameButton()
+}
 
 
 function dragStart(){
-    currTile = this;
+    currTile = this
 }
 
 function dragOver(e){
-    e.preventDefault();
+    e.preventDefault()
 }
 
 function dragEnter(e){
-    e.preventDefault();
+    e.preventDefault()
 }
 
 function dragLeave(){
@@ -78,20 +116,21 @@ function dragLeave(){
 }
 
 function dragDrop(){
-    otherTile = this;
+    otherTile = this
 }
 
 function dragEnd(){
-    if (currTile.src.includes("prazno")){
-        return;
+    if (currTile.src.includes("empty")){
+        return
     }
-    let currImg = currTile.src;
-    let otherImg = otherTile.src;
-    currTile.src = otherImg;
-    otherTile.src = currImg;
 
-    broj_poteza += 1;
-    document.getElementById("brpoteza").innerText=broj_poteza;
+    let currImg = currTile.src
+    let otherImg = otherTile.src
+    currTile.src = otherImg
+    otherTile.src = currImg
+
+    movesCounter += 1
+    document.getElementById("moves-counter").innerText = movesCounter
 }
 
 
